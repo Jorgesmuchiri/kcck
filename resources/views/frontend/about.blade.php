bf14703dd3d0712541064efed1415566e5bcb489
George Ndungu <georgendungu.muchiri@strathmore.edu>
2020-04-22 06:10:28 +0000

    about.blade.php edited online with Bitbucket

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -228,6 +228,8 @@ The curriculum is designed to help learners think computationally which is benef
 
 
   <section class="teacher_section layout_padding-bottom">
+  <div class="row">
+   <div class="column">
     <div class="container">
       <h2 class="main-heading " style="color: #b42025;">
         Our Team
@@ -270,8 +272,9 @@ The curriculum is designed to help learners think computationally which is benef
       </div>
 
 
-   
+   </div>
     </div>
+       </div>
   </section>
 
 
