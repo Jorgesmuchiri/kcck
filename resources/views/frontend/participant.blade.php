46f7ddd0ae8ba8a4ad91b8fcf9fc907e70047ae0
George Ndungu <georgendungu.muchiri@strathmore.edu>
2020-04-28 15:51:50 +0000

    participant.blade.php edited online with Bitbucket

--- resources/views/frontend/participant.blade.php
+++ resources/views/frontend/participant.blade.php
@@ -41,6 +41,19 @@
 <body>
 
 
+	 @if(Session::has('success'))
+  <script type="text/javascript">
+     swal({
+         title:'Success!',
+         text:"{{Session::get('success')}}",
+         timer:5000,
+         type:'success'
+     }).then((value) => {
+       //location.reload();
+     }).catch(swal.noop);
+ </script>
+ @endif
+
 <style>input[type=text], select, textarea{
   width: 100%;
   padding: 12px;
