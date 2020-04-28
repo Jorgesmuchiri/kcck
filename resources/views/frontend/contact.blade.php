cad4192255be2f6ae7faf6e1711143639ee80add
George Ndungu <georgendungu.muchiri@strathmore.edu>
2020-04-28 15:51:24 +0000

    contact.blade.php edited online with Bitbucket

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -43,6 +43,18 @@
 </head>
 
 <body>
+	 @if(Session::has('success'))
+  <script type="text/javascript">
+     swal({
+         title:'Success!',
+         text:"{{Session::get('success')}}",
+         timer:5000,
+         type:'success'
+     }).then((value) => {
+       //location.reload();
+     }).catch(swal.noop);
+ </script>
+ @endif
   <div class="top_container sub_pages ">
     <!-- header section strats -->
     <header class="header_section">
